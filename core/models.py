816176e9953da40cc5ac2fb863dd8281fc357cd4
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-03-15 02:22:19 +0530

    [UPDATE] All Auth & Models & list all items

--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    '''
+    All Items
+    '''
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+class OrderItem(models.Model):
+    '''
+    Link between Items and Orders
+    '''
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Order(models.Model):
+    '''
+    All Orders and Cart Items
+    '''
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
--- djecommerce/urls.py
+++ djecommerce/urls.py
@@ -5,10 +5,17 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('core.urls', namespace='core'))
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
+    # urlpatterns += static(settings.STATIC_URL,
+    #                       document_root=settings.STATIC_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL,
+    #                       document_root=settings.MEDIA_ROOT)
