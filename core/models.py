49cc9f9aff19ae7ec6680940c9e2b4d8b89981d4
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-03-17 20:42:38 +0530

    [UPDATE] add to cart feature

--- core/models.py
+++ core/models.py
@@ -22,9 +22,11 @@ class Item(models.Model):
     '''
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=2)
     slug = models.SlugField()
+    description = models.TextField()
 
     def __str__(self):
         return self.title
@@ -34,14 +36,23 @@ class Item(models.Model):
             'slug': self.slug
         })
 
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
 class OrderItem(models.Model):
     '''
     Link between Items and Orders
     '''
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                                    on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.title
+        return f"{self.quantity} of {self.item.title}"
 
 class Order(models.Model):
     '''
